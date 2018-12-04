be55090f7d7f9e5ccb1cad7a622bcba12385f628
eegod <9635792+eegod@users.noreply.github.com>
2018-12-04 13:12:00 +0800

    rename apk files

--- release.sh
+++ release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+mkdir -p apk
 
 release=$1
-cp mobile/build/outputs/apk/release/mobile-armeabi-v7a-release.apk shadowsocks-armeabi-v7a-${release}.apk
-cp mobile/build/outputs/apk/release/mobile-arm64-v8a-release.apk shadowsocks-arm64-v8a-${release}.apk
-cp mobile/build/outputs/apk/release/mobile-x86-release.apk shadowsocks-x86-${release}.apk
-cp mobile/build/outputs/apk/release/mobile-universal-release.apk shadowsocks--universal-${release}.apk
+cp mobile/build/outputs/apk/release/mobile-armeabi-v7a-release.apk apk/shadowsocks-armeabi-v7a-${release}.apk
+cp mobile/build/outputs/apk/release/mobile-arm64-v8a-release.apk apk/shadowsocks-arm64-v8a-${release}.apk
+cp mobile/build/outputs/apk/release/mobile-x86-release.apk apk/shadowsocks-x86-${release}.apk
+cp mobile/build/outputs/apk/release/mobile-universal-release.apk apk/shadowsocks--universal-${release}.apk
